8c5888629f3830d1711f6ce4ba4b4ab136fd3d18
chacho8800 <pchachoyan@gmail.com>
2025-10-15 19:12:40 -0700

    Complete CRUD functionality

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('parks/', views.parks, name='parks-index'),
-    path('parks/<int:park_id>/', views.park_detail, name='park-detail')
+    path('parks/<int:park_id>/', views.park_detail, name='park-detail'),
+    path('parks/create/', views.ParkCreate.as_view(), name='park-create'),
+    path('park/<int:pk>/update', views.ParkUpdate.as_view(), name='park-update'),
+    path('park/<int:pk>/delete', views.ParkDelete.as_view(), name='park-delete')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import NationalParks
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 def home(request):  # home 
@@ -10,6 +11,19 @@ def parks(request): # index
     parks = NationalParks.objects.all()
     return render(request, 'parks/index.html', {'parks' : parks})
 
-def park_detail(request, park_id):
+def park_detail(request, park_id): # detail
     park = NationalParks.objects.get(id=park_id)
     return render(request, 'parks/detail.html', {'park': park} )
+
+class ParkCreate(CreateView): # create
+    model = NationalParks
+    fields = '__all__'
+
+
+class ParkUpdate(UpdateView): # update
+    model = NationalParks
+    fields = '__all__'
+
+class ParkDelete(DeleteView): # delete
+    model = NationalParks
+    success_url = '/parks/'
